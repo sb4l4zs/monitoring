7a88a0075d67d35887ef433752ecabff5642d1c1
Suru Balazs <surubalazs@gmail.com>
2026-09-07 21:03:12 +0200

    Task2 set entity relationships

--- src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
+++ src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
@@ -21,6 +21,12 @@ public class Sensor {
   @Column(nullable = false)
   private SensorType type;
 
+  @OneToMany(mappedBy = "sensor")
+  private List<Alert> alerts;
+
+  @OneToMany(mappedBy = "sensor")
+  private List<SensorReading> sensorReadings;
+
   public Sensor() {}
 
   public Sensor(String name, SensorType type) {
@@ -53,22 +59,20 @@ public class Sensor {
   }
 
   public List<Alert> getAlerts() {
-    //TODO: Task 2
-    return null;
+    return alerts;
   }
 
   public void setAlerts(List<Alert> alerts) {
-    //TODO: Task 2
+    this.alerts = alerts;
   }
 
   public List<SensorReading> getSensorReadings() {
-    //TODO: Task 2
-    return null;
+    return sensorReadings;
   }
 
   public void setSensorReadings(
       List<SensorReading> sensorReadings) {
-    //TODO: Task 2
+    this.sensorReadings = sensorReadings;
   }
 
   @Override
--- src/main/java/com/aldisued/iot/monitoring/entity/SensorReading.java
+++ src/main/java/com/aldisued/iot/monitoring/entity/SensorReading.java
@@ -1,11 +1,7 @@
 package com.aldisued.iot.monitoring.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
 @Table(name = "sensor_readings")
@@ -22,6 +18,10 @@ public class SensorReading {
   @Column(nullable = false)
   private LocalDateTime timestamp;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sensor_id")
+  private Sensor sensor;
+
   public SensorReading() {
   }
 
@@ -32,7 +32,7 @@ public class SensorReading {
   ) {
     this.value = value;
     this.timestamp = timestamp;
-    //TODO: Task 2
+    this.sensor = sensor;
   }
 
   public Long getId() {
@@ -60,11 +60,10 @@ public class SensorReading {
   }
 
   public Sensor getSensor() {
-    //TODO: Task 2
-    return null;
+    return sensor;
   }
 
   public void setSensor(Sensor sensor) {
-    //TODO: Task 2
+    this.sensor = sensor;
   }
 }
