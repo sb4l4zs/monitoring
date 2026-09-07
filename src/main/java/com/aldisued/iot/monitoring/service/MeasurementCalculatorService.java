06eee4818d36cbde1936c8b3cc977b6d08cbecb4
Suru Balazs <surubalazs@gmail.com>
2026-09-07 22:11:38 +0200

    Task9 impl filterByAverageDeviation

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -1,20 +1,32 @@
 package com.aldisued.iot.monitoring.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MeasurementCalculatorService {
 
-  public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    // TODO: Task 9
-    return List.of();
-  }
+    public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
+        if (deviation < 0.0 || deviation > 1.0) {
+            throw new IllegalArgumentException("value of deviation must be between 0.0 and 1.0");
+        }
 
-  public List<Double> getMovingAverage(List<Double> data, int windowSize) {
-    // TODO: Task 10
-    return List.of();
-  }
+        double avg = values.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
+
+        return values.stream()
+                .filter(value -> Math.abs(value - avg) <= deviation * avg)
+                .toList();
+    }
+
+    public List<Double> getMovingAverage(List<Double> data, int windowSize) {
+        // TODO: Task 10
+        return List.of();
+    }
 
 }
